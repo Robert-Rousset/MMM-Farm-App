41b665abcfd30652e43e5be7dce14f3cbde402a2
Robert Rousset <whyisrob@hotmail.com>
2021-04-15 20:22:39 +0800

    Buttons creating a list and clear buttons working

---
+++ Assets/script.js
@@ -0,0 +1,55 @@
+var eggSize = document.querySelector('#eggSize')
+var eggNumber = document.querySelector('#eggNumber')
+var orderUpButton = document.querySelector('#orderUp')
+
+var bagNumber = document.querySelector('#bagNumber')
+var gimmeBagsButton = document.querySelector('#bagsButton')
+
+var chickenNumber = document.querySelector('#chickenNumber')
+var chickenButton = document.querySelector('#chickenButton')
+
+var orderList = document.querySelector('.orderList')
+
+orderUpButton.addEventListener('click', orderUpFunction)
+function orderUpFunction(){
+    // creating an element p //
+    var eggOrderList = document.createElement('p')
+    eggOrderList.textContent = eggNumber.value + " " + eggSize.value +" "
+    // appending this p item to the orderList
+    orderList.append(eggOrderList)
+    var clearButton = document.createElement('button')
+    clearButton.classList.add('clearButton')
+    eggOrderList.append(clearButton)
+    clearButton.addEventListener('click', clearButtonFunction)
+    function clearButtonFunction(){
+        eggOrderList.remove()
+        clearButton.remove()
+}}
+
+gimmeBagsButton.addEventListener('click', gimmeBagsFunction)
+function gimmeBagsFunction(){
+    var bagOrderList = document.createElement('p')
+    bagOrderList.textContent = bagNumber.value + " Mannure Bags ";
+    orderList.append(bagOrderList)
+    var clearButton = document.createElement('button')
+    clearButton.classList.add('clearButton')
+    bagOrderList.append(clearButton)
+    clearButton.addEventListener('click', clearButtonFunction)
+    function clearButtonFunction(){
+        bagOrderList.remove()
+        clearButton.remove()
+}}
+
+chickenButton.addEventListener('click', chickenButtonFunction)
+function chickenButtonFunction(){
+    var chickenOrderList = document.createElement('p')
+    chickenOrderList.textContent = chickenNumber.value + " Chickens "
+    orderList.append(chickenOrderList)
+    var clearButton = document.createElement('button')
+    clearButton.classList.add('clearButton')
+    chickenOrderList.append(clearButton)
+    clearButton.addEventListener('click', clearButtonFunction)
+    function clearButtonFunction(){
+        chickenOrderList.remove()
+        clearButton.remove()
+}}
